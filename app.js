842ffc9ba4899c912ca2632bd64ef062792c81d6
wali55 <walisantunu@gmail.com>
2023-10-26 12:03:37 +0600

    finish poll app

--- app.js
+++ app.js
@@ -14,12 +14,16 @@ app.use(express.json());
 app.get('/create', pollController.createPollGetController);
 app.post('/create', pollController.createPollPostController);
 
+app.get('/polls/:id', pollController.viewPollGetController);
+app.post('/polls/:id', pollController.viewPollPostController);
+app.get('/polls', pollController.getAllPolls);
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/test")
+  .connect("mongodb://127.0.0.1:27017/express-cc")
   .then(() => {
     app.listen(4545, () => {
       console.log("app is listening on port 4545");
--- pollController.js
+++ pollController.js
@@ -1,9 +1,74 @@
+const Poll = require('./Poll');
+
 exports.createPollGetController = (req, res, next) => {
     res.render('create');
 };
-
-exports.createPollPostController = (req, res, next) => {
+exports.createPollPostController = async (req, res, next) => {
     let { title, description, options } = req.body;
-
-    res.render('create');
+    options = options.map((option) => {
+        return {
+            name: option,
+            vote: 0,
+        }
+    });
+    let poll = new Poll({
+        title,
+        description,
+        options,
+    });
+    try {
+        await poll.save();
+        res.redirect('/polls');
+    } catch (error) {
+        console.log(error);
+    }
 };
+
+exports.getAllPolls = async (req, res, next) => {
+    try {
+        let polls = await Poll.find();
+        res.render('polls', { polls });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.viewPollGetController = async (req, res, next) => {
+    let id = req.params.id;
+    try {
+        let poll = await Poll.findById(id);
+        let options = [...poll.options];
+        let result = [];
+        options.forEach((option) => {
+            let percentage = (option.vote * 100) / poll.totalVote;
+            result.push({
+                ...option._doc,
+                percentage: percentage ? percentage : 0
+            })
+        });
+
+        res.render('viewPoll', { poll, result })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.viewPollPostController = async (req, res, next) => {
+    let id = req.params.id;
+    let optionId = req.body.option;
+    try {
+        let poll = await Poll.findById(id);
+        let options = [...poll.options];
+        let index = options.findIndex((option) => option.id === optionId);
+        options[index].vote++;
+        let totalVote = poll.totalVote + 1;
+
+        await Poll.findOneAndUpdate(
+            { _id: poll._id },
+            { $set: {options, totalVote} }
+        );
+        res.redirect('/polls/' + id);
+    } catch (error) {
+        console.log(error);
+    }
+}
